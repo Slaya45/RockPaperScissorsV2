662dcaf5a1945a5c60aeaf363f27f8be20453b91
Slaya45 <caydenfrye@gmail.com>
2023-03-10 20:32:38 -0700

    Added a background and made the text easier to read, also changed the scissors img

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let playerScore = 0
 let computerScore = 0
 
 
+
 function buttonRock(){
     playerChoice =  "Rock"
     playerChoiceEl.textContent = "Player:" + " " + "Rock"
@@ -32,6 +33,7 @@ function buttonPaper(){
     result()
     
 }
+
 
 function computerChoice(){
 
